213649090a35a4ee6003c6d91e3407c8c6b4fe3a
Adam Goth <adamgoth@gmail.com>
2016-10-12 18:12:31 -0600

    Configures Firebase

---
+++ Pods/Target Support Files/Pods-tiny-tabs/Pods-tiny-tabs-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tiny_tabs : NSObject
+@end
+@implementation PodsDummy_Pods_tiny_tabs
+@end
---
+++ Pods/Target Support Files/Pods-tiny-tabs/Pods-tiny-tabs-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_tiny_tabsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_tiny_tabsVersionString[];
+
