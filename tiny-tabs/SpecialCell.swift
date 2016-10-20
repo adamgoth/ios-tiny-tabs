835efcb07ed7bbdc2e9b54e1effae043e0c814df
Adam Goth <adamgoth@gmail.com>
2016-10-20 17:07:23 -0600

    Updates UI styling

--- tiny-tabs/SpecialCell.swift
+++ tiny-tabs/SpecialCell.swift
@@ -21,7 +21,7 @@ class SpecialCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
