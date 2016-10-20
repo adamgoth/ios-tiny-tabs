f0e4ad3cb614f2ac8b5f5bb61492d153756f2594
Adam Goth <adamgoth@gmail.com>
2016-10-20 17:06:23 -0600

    Adds custom view class for special cell container view

---
+++ tiny-tabs/SpecialContainer.swift
@@ -0,0 +1,19 @@
+//
+//  SpecialContainer.swift
+//  tiny-tabs
+//
+//  Created by Adam Goth on 10/20/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import UIKit
+
+class SpecialContainer: UIView {
+    
+    override func awakeFromNib() {
+        
+        layer.cornerRadius = 5.0
+        layer.borderWidth = 3.0
+    }
+
+}
