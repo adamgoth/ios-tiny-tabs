acb7e7f5777644e220a7bb72448798428d2952a2
Adam Goth <adamgoth@gmail.com>
2016-10-21 07:12:36 -0600

    Creates custom button class and updates filter buttons appearance

---
+++ tiny-tabs/FilterButton.swift
@@ -0,0 +1,18 @@
+//
+//  FilterButton.swift
+//  tiny-tabs
+//
+//  Created by Adam Goth on 10/20/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import UIKit
+
+class FilterButton: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+        //layer.borderWidth = 2.0
+    }
+
+}
